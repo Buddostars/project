8044eb5d1ef81477e696a53bb1293e0b75e5a890
Michael Albrecht <michael.j.a@hotmail.de>
2024-09-24 20:50:46 +1000

    rm open_gl folder

---
+++ build/CMakeFiles/OpenGLProject.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/michaelalbrecht/Studium_Dateien/wise2425/Computer Graphics/project/open_gl/glad.c" "CMakeFiles/OpenGLProject.dir/glad.c.o" "gcc" "CMakeFiles/OpenGLProject.dir/glad.c.o.d"
+  "/Users/michaelalbrecht/Studium_Dateien/wise2425/Computer Graphics/project/open_gl/controls.cpp" "CMakeFiles/OpenGLProject.dir/controls.cpp.o" "gcc" "CMakeFiles/OpenGLProject.dir/controls.cpp.o.d"
+  "/Users/michaelalbrecht/Studium_Dateien/wise2425/Computer Graphics/project/open_gl/main.cpp" "CMakeFiles/OpenGLProject.dir/main.cpp.o" "gcc" "CMakeFiles/OpenGLProject.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
