0c592b1548b7214b8671d370cde405b1fc7360e1
Michael Albrecht <michael.j.a@hotmail.de>
2024-09-27 07:55:28 +0000

    changed filestructure to have *.dll in root and del unnessesary also minimal task.json

---
+++ TODO/main_minimal.cpp
@@ -0,0 +1,36 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Test Window", NULL, NULL);
+    if (!window) {
+        const char* description;
+        int code = glfwGetError(&description);
+        if (code != GLFW_NO_ERROR) {
+            std::cerr << "GLFW Error: " << description << std::endl;
+        }
+
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -118,7 +118,10 @@ void processNode(aiNode* node, const aiScene* scene) {
 
 int main(){
     // Initialize GLFW
-    glfwInit();
+    if (!glfwInit()) {
+    std::cout << "Failed to initialize GLFW" << std::endl;
+    return -1;
+    }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
